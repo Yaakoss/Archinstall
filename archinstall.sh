5d6ae853287fe494b4eef38c0a4ee9bd795fe12f
Yaakoss <patricia@rakmar.de>
2024-04-01 22:50:24 +0200

    added some lines

--- archinstall.sh
+++ archinstall.sh
@@ -14,13 +14,20 @@ loadkeys $KiEYBOARD_LAYOUT
 sed -i 's/#Color/Color/g' /etc/pacman.conf
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 20/g' /etc/pacman.conf
 sed -i -z 's/#\[multilib\]\n#Include/\[multilib\]\nInclude/' /etc/pacman.conf
+echo -ne "
+Checking mirrors
+"
 reflector --country $COUNTRY_LIST --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+echo -ne "
+Partitioning Disks
+"
 sgdisk -Z $DISK
 sgdisk $DISK -n 1::+1GiB -t 1:ef00
 sgdisk $DISK -n 2::
 sgdisk -p $DISK
-ROOT_PARTITION=$DISK"2"
-echo $ROOT_PARTITION
+export ROOT_PARTITION=$DISK"2"
+echo "ROOT PARTITION=$ROOT_PARTITION"
+
 pause
 echo -n $Crypt_Password | cryptsetup -q luksFormat --label Arch $ROOT_PARTITION -
 echo -n $Crypt_Password | cryptsetup -q luksOpen $ROOT_PARTITION $CRYPT_DEVICE -
