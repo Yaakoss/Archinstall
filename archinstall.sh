6fc6bf79b2ad824fda750e2e77aa970f61af46cd
Yaakoss <patricia@rakmar.de>
2024-04-01 20:58:10 +0200

    added some lines

--- archinstall.sh
+++ archinstall.sh
@@ -15,3 +15,9 @@ sed -i 's/#Color/Color/g' /etc/pacman.conf
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 20/g' /etc/pacman.conf
 sed -i -z 's/#\[multilib\]\n#Include/\[multilib\]\nInclude/' /etc/pacman.conf
 reflector --country France,Germany --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sgdisk -Z $DISK
+sgdisk $DISK -n 1::+1GiB -t 1:ef00
+sgdisk $DISK -n 2::
+sgdisk -p $DISK
+ROOT_PARTITON=$DISK"2"
+cryptsetup -q luksFormat --label Arch $ROOT_PARTITION $Crypt_Password
