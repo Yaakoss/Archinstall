ba78ac53139309fd5db8cc838b848b0c8d603aed
Yaakoss <patricia@rakmar.de>
2024-04-01 22:33:52 +0200

    added some lines

--- archinstall.sh
+++ archinstall.sh
@@ -10,14 +10,21 @@ _______________________________________
 _______________________________________"
 
 source archinstall.conf
-loadkeys $Keyboard_Layout
+loadkeys $KiEYBOARD_LAYOUT
 sed -i 's/#Color/Color/g' /etc/pacman.conf
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 20/g' /etc/pacman.conf
 sed -i -z 's/#\[multilib\]\n#Include/\[multilib\]\nInclude/' /etc/pacman.conf
-reflector --country France,Germany --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --country $COUNTRY_LIST --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 sgdisk -Z $DISK
 sgdisk $DISK -n 1::+1GiB -t 1:ef00
 sgdisk $DISK -n 2::
 sgdisk -p $DISK
 ROOT_PARTITON=$DISK"2"
-cryptsetup -q luksFormat --label Arch $ROOT_PARTITION $Crypt_Password
+echo -n $Crypt_Password | cryptsetup -q luksFormat --label Arch $ROOT_PARTITION -
+echo -n $Crypt_Password | cryptsetup -q luksOpen $ROOT_PARTITION $CRYPT_DEVICE -
+pvcreate /dev/mapper/$CRYPT_DEVICE
+vgcreate $VOLUME_GROUP /dev/mapper/$VOLUME_GROUP
+lvcreate -n swap -L10G $VOLUME_GROUP
+lvcreate -n root -l100%FREE $VOLUME_GROUP
+mkfs.fat -n Efi -F32 $ROOT_PARTITION"1"
+mkfs.btrfs -L Root /dev/mapper/$VOLUME_GROUP
