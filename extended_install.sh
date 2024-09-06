24b97d698a1365bd56698ef84c3dbf8a60f64a23
Yaakoss <138389047+Yaakoss@users.noreply.github.com>
2024-09-06 06:22:36 +0200

    Update extended_install.sh
    removed duplicates

--- extended_install.sh
+++ extended_install.sh
@@ -7,7 +7,7 @@ git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si
 paru -S xorg-server xorg-xinit qtile qtile-extras cantarell-fonts wezterm keepassxc variety python-dbus-next python-psutil noto-color-emoji-fontconfig pipewire pipewire-pulse blueman bolt mesa thorium-browser pasystray rofi-adi1090x pavucontrol
-paru -S pasystray pavucontrol pipewire-pulse arandr inetutils nitrogen fastfetch xdg-user-dirs nextcloud-client xaskpass
+paru -S pasystray pipewire-pulse arandr inetutils nitrogen xdg-user-dirs nextcloud-client xaskpass
 exit
 cat >> /etc/X11/xorg.conf.d/00-keyboard.conf << EOF
 Section "InputClass"
